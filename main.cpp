a2f4216d91bbc20f156532438eadcad81aea5be0
mgalactico <mjarrin@outlook.com>
2017-01-12 21:38:37 -0500

    Added logic for Exercise 2 Chapter 6.

--- main.cpp
+++ main.cpp
@@ -64,7 +64,7 @@ Token Token_stream::get()
     switch (ch) {
     case ';':    // for "print"
     case 'q':    // for "quit"
-    case '(': case ')': case '+': case '-': case '*': case '/':
+    case '(': case ')': case '+': case '-': case '*': case '/': case '{': case '}':
         return Token(ch);        // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
@@ -100,6 +100,13 @@ double primary()
             double d = expression();
             t = ts.get();
             if (t.kind != ')') error("')' expected");
+            return d;
+        }
+        case '{':    // handle '(' expression ')'
+        {
+            double d = expression();
+            t = ts.get();
+            if (t.kind != '}') error("')' expected");
             return d;
         }
     case '8':            // we use '8' to represent a number
